5afc8d8b5336ebc07685b69586fdd1e34e9a3ecc
RedwanAhmedTapuu <redwan25880@gmail.com>
2025-02-25 00:23:01 +0600

    server updated

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,11 +84,14 @@ WSGI_APPLICATION = 'chatbot_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="postgresql://root:RkxxJE2BODtsB5bkrE2Xhz0MpZVbNNFw@dpg-cuuafslds78s73amohh0-a.oregon-postgres.render.com/chat_bot_database",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+
 
 
 # Password validation
